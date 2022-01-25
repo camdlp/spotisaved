be781a950a539d8e39c49e9e9d111f0acbf34f48
Carlos Abia <carlosabiamer@gmail.com>
2022-01-25 20:25:40 +0100

    Gathering Spotify API info from JSON file

--- refresh.py
+++ refresh.py
@@ -1,18 +1,21 @@
 from email.mime import base
-from secrets import refresh_token, base_64
 import requests
 import json
+from secrets import Secrets
+
 
 class Refresh:
-    
+
     def __init__(self):
-        self.refresh_token = refresh_token
-        self.base_64 = base_64
-        
+        #self.refresh_token = refresh_token
+        #self.base_64 = base_64
+        self.s = Secrets()
+
     def refresh(self):
         query = "https://accounts.spotify.com/api/token"
-        response = requests.post(query, 
-                                 data={"grant_type": "refresh_token", "refresh_token": refresh_token},
-                                 headers={"Authorization": "Basic "+ base_64})
+        response = requests.post(query,
+                                 data={"grant_type": "refresh_token", "refresh_token": self.s.refresh_token},
+                                 headers={"Authorization": "Basic " + self.s.base_64})
         response_json = response.json()
+        print(response_json)
         return response_json["access_token"]
